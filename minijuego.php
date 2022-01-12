368b4894ff50d7d3357e10f190634f11d92c6292
d <dnuezsan@gmail.com>
2022-01-13 00:22:07 +0100

    documento word y la insercion actualizados

--- minijuego.php
+++ minijuego.php
@@ -12,14 +12,28 @@ class Minijuegos
         $this->conexion = new mysqli(SERVIDOR, USUARIO, CONTRASENIA, BD);
     }
 
-    function insercion()
+   /*  function insercion()
     {
         $sql = "INSERT INTO minijuego ('idMinijuego', 'nombre', 'ruta, 'portada', 'fechaHora) VALUES (?, ?, ?, ?, ?)";
 
         $consulta = $this->conexion->prepare($sql);
 
         $resultado = $consulta->$consulta;
-    }
+    } */
 
+    function insercion()
+    {
+        $sql = "INSERT INTO minijuego ('idMinijuego', 'nombre') VALUES (?, ?)";
+
+        $consulta_prep = $this->conexion->prepare($sql);
+
+        $consulta_prep->bind_param('is', $_POST['idMinijuego'], $_POST['nombre']);
+
+        if ($consulta_prep->execute()) {
+            echo "Se ha realizado la inserción";
+        }
+
+        $consulta_prep->close();
+    }
 
 }
